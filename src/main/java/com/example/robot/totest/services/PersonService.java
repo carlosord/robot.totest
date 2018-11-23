5474c6b790b707ea32ad56d0073f2fc4a9e7aaff
Carlos Ordonez <Carlos.Ordonez@ricoh.es>
2018-11-23 14:46:31 +0100

    [add-services] Basic services added

---
+++ src/main/java/com/example/robot/totest/services/PersonService.java
@@ -0,0 +1,8 @@
+package com.example.robot.totest.services;
+
+/**
+ * The Interface PersonService.
+ */
+public interface PersonService {
+
+}
---
+++ src/main/java/com/example/robot/totest/services/impl/PersonServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.robot.totest.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.robot.totest.services.PersonService;
+
+/**
+ * The Class PersonServiceImpl.
+ */
+@Service
+public class PersonServiceImpl implements PersonService{
+
+}
