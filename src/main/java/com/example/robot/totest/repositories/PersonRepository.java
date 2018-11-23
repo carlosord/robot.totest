ddf033faaa9c07fd67323a262f104feb6c9e0c81
Carlos Ordonez <Carlos.Ordonez@ricoh.es>
2018-11-23 14:31:11 +0100

    [add-repositories] Added spring annotations and javadoc to repositories

--- src/main/java/com/example/robot/totest/repositories/PersonRepository.java
+++ src/main/java/com/example/robot/totest/repositories/PersonRepository.java
@@ -3,11 +3,22 @@ package com.example.robot.totest.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.robot.totest.model.Person;
 
+/**
+ * The Interface PersonRepository.
+ */
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 	
+	/**
+	 * Find by dni.
+	 *
+	 * @param dni the dni
+	 * @return the list
+	 */
 	public List<Person> findByDni(String dni);
 
 }
