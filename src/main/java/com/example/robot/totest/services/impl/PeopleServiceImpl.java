f16bd902b5bee1651eddb89e93c4e2fe6b064fc8
Carlos Ordóñez <carlos.ordonez@ricoh.es>
2018-12-04 08:19:34 +0100

    [performance] Added transactional services and new robot test

--- src/main/java/com/example/robot/totest/services/impl/PeopleServiceImpl.java
+++ src/main/java/com/example/robot/totest/services/impl/PeopleServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.robot.totest.dto.PersonDto;
 import com.example.robot.totest.model.Associations;
@@ -18,6 +19,7 @@ import com.example.robot.totest.services.PeopleService;
  * The Class PeopleServiceImpl.
  */
 @Service
+@Transactional
 public class PeopleServiceImpl implements PeopleService{
 
 	/** The person repository. */
